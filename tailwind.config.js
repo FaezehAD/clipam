b16617ec8159cbbc14eb6ffc50ee34a98b4446fd
FaezehAD <FaezehAD2001@gmail.com>
2022-08-25 12:11:34 +0430

    final version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,46 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/pages/index.html"],
+  purge: {
+    content: ["./src/pages/*.html"],
+  },
   theme: {
-    extend: {},
+    screens: {
+      
+      // mobile
+      'xs': '475px',
+
+      // tablet
+      'sm': '640px',
+      'md': '768px',
+      
+      //desktop
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1700px',
+      '4xl': '1950px',
+
+    },
+    extend: {
+      colors: {
+        indigo: "#6F88FC",
+        indigoDark: "#627ADE",
+        lightBlue:'#5a99fe33',
+        borderBlue:'#8de1fe',
+        borderGray:'#324d784d',
+      },
+      spacing: {
+        '100':'27rem',
+        '104': '30rem',
+        '120': '40rem',
+      },
+      borderWidth: {
+        '40': '40px',
+      }
+  
+  
+    },
   },
   plugins: [],
-}
+};
